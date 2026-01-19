92825c9c2e3bb32b7cc526f0e548b864ccb039cd
muhammed-sahal-k <sahalkmohammed95@gmail.com>
2026-01-19 13:00:36 +0530

    second commit

--- connection.js
+++ connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export default async function connection(){
 
-    const db = await mongoose.connect('mongodb://mongodb+srv://sahalkmohammed95_db_user:LIQEG1zCyXwkNGSZ@cluster0.wqrxgcv.mongodb.net//BACKENDEXPRESS')
+    const db = await mongoose.connect('mongodb://mongodb+srv://sahalkmohammed95_db_user:LIQEG1zCyXwkNGSZ@cluster0.wqrxgcv.mongodb.net/BACKENDEXPRESS')
     console.log("database created");
 
     return db
